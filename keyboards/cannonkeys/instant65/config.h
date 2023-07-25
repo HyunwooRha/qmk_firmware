66f51a01680d3aee1753e26d9c421498b0a56e24
HyunwooRha <82012231+HyunwooRha@users.noreply.github.com>
2023-07-24 22:29:12 -0700

    test 1

--- keyboards/cannonkeys/instant65/config.h
+++ keyboards/cannonkeys/instant65/config.h
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define MK_KINETIC_SPEED
+#define MK_COMBINED
 #define TAPPING_TERM 200
 
 #define BACKLIGHT_PWM_DRIVER    PWMD3
