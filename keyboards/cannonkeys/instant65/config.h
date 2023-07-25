4315e61624db983cad411be53a7163c869e4cb15
HyunwooRha <82012231+HyunwooRha@users.noreply.github.com>
2023-07-24 23:52:18 -0700

    test 2

--- keyboards/cannonkeys/instant65/config.h
+++ keyboards/cannonkeys/instant65/config.h
@@ -18,7 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define MK_COMBINED
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
 
 #define BACKLIGHT_PWM_DRIVER    PWMD3
 #define BACKLIGHT_PWM_CHANNEL   1
--- keyboards/cannonkeys/instant65/keymaps/kenetic_constant_mouse_mode/keymap.c
+++ keyboards/cannonkeys/instant65/keymaps/kenetic_constant_mouse_mode/keymap.c
@@ -2,42 +2,34 @@
 #include QMK_KEYBOARD_H
 #define TAPPING_TOGGLE 2
 
+#include QMK_KEYBOARD_H
+
 typedef struct {
   bool is_press_action;
   int state;
 } tap;
 
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  TRIPLE_TAP = 5,
+  TRIPLE_HOLD = 6
+};
+
+//Tap dance enums
+enum {
+  ALT_OSL4 = 0
+};
+
 int cur_dance (qk_tap_dance_state_t *state);
 void alt_finished (qk_tap_dance_state_t *state, void *user_data);
 void alt_reset (qk_tap_dance_state_t *state, void *user_data);
-	
-
-enum layer_names {
-    _BASE,
-    1,
-	2,
-	3,
-	4
-};
-
-enum {
-	SINGLE_TAP = 1,
-	SINGLE_HOLD = 2,
-	DOUBLE_TAP = 3,
-	DOUBLE_HOLD = 4,
-	TRIPLE_TAP = 5,
-	TRIPLE_HOLD = 6
-
-};
-
-enum {
-	ALT_OSL4 = 0
-};
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_BASE] = LAYOUT_default(
+	[0] = LAYOUT(
 		// KC_ESC,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINS,KC_EQL,KC_BSPC,KC_INS,
 		// KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_LBRC,KC_RBRC,KC_BSLS,KC_DEL,
 		// KC_CAPS,KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCLN,KC_QUOT,KC_ENT,KC_PGUP,
@@ -50,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, MO(1),   TD(ALT_OSL4),                   KC_SPC,                      TT(2),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 
     ),
-	[1] = LAYOUT_default(
+	[1] = LAYOUT(
 		KC_GRAVE, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_HOME,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_LEFT, KC_DOWN, KC_RIGHT,KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
 		TT(3),   KC_NO,   KC_LGUI,                   KC_SPC,                             KC_TRNS, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 	),
-    [2] = LAYOUT_default(
+    [2] = LAYOUT(
 		KC_GRAVE, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_HOME,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
@@ -65,14 +57,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_NO,   KC_LALT,                   KC_SPC,                             KC_TRNS, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	//mouse layer
-	[3] = LAYOUT_default(
+	[3] = LAYOUT(
 		KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_HOME,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_CAPS, KC_ACL0, KC_ACL1, KC_ACL2, KC_F,    KC_G,    KC_H,    KC_MS_LEFT,   KC_MS_DOWN,  KC_MS_RIGHT,   KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
 		KC_TRNS, KC_NO,   KC_NO,                   KC_SPC,                             KC_NO, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     )
-	[4] = LAYOUT_default(
+	[4] = LAYOUT(
 		KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_HOME,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
@@ -80,6 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI, TD(ALT_OSL4),                   RESET,                      KC_NO,   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 	)
 };
+
 
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
@@ -105,10 +98,10 @@ static tap alttap_state = {
 void alt_finished (qk_tap_dance_state_t *state, void *user_data) {
   alttap_state.state = cur_dance(state);
   switch (alttap_state.state) {
-    case SINGLE_TAP: set_oneshot_layer(1, ONESHOT_START); clear_oneshot_layer_state(ONESHOT_PRESSED); break;
+    case SINGLE_TAP: set_oneshot_layer(4, ONESHOT_START); clear_oneshot_layer_state(ONESHOT_PRESSED); break;
     case SINGLE_HOLD: register_code(KC_LALT); break;
-    case DOUBLE_TAP: set_oneshot_layer(1, ONESHOT_START); set_oneshot_layer(1, ONESHOT_PRESSED); break;
-    case DOUBLE_HOLD: register_code(KC_LALT); layer_on(1); break;
+    case DOUBLE_TAP: set_oneshot_layer(4, ONESHOT_START); set_oneshot_layer(4, ONESHOT_PRESSED); break;
+    case DOUBLE_HOLD: register_code(KC_LALT); layer_on(4); break;
     //Last case is for fast typing. Assuming your key is `f`:
     //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
     //In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
@@ -120,13 +113,13 @@ void alt_reset (qk_tap_dance_state_t *state, void *user_data) {
     case SINGLE_TAP: break;
     case SINGLE_HOLD: unregister_code(KC_LALT); break;
     case DOUBLE_TAP: break;
-    case DOUBLE_HOLD: layer_off(1); unregister_code(KC_LALT); break;
+    case DOUBLE_HOLD: layer_off(4); unregister_code(KC_LALT); break;
   }
   alttap_state.state = 0;
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [ALT_OSL1]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,alt_finished, alt_reset)
+  [ALT_OSL4]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,alt_finished, alt_reset)
 };
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
