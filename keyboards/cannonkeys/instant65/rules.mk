de814538908691d09cbeb05e0a1df8f3b549cc5e
HyunwooRha <82012231+HyunwooRha@users.noreply.github.com>
2025-08-04 22:51:15 -0700

    Update rules.mk

--- keyboards/cannonkeys/instant65/rules.mk
+++ keyboards/cannonkeys/instant65/rules.mk
@@ -12,8 +12,8 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 TAP_DANCE_ENABLE = yes 	    # Tap dance
 # Enter lower-power sleep mode when on the ChibiOS idle thread
