7671712ab2931297272eaca76a2059422153c08d
HyunwooRha <hyunwoorha@gmail.com>
2025-08-25 17:55:10 -0700

    Update keymap.c

--- keyboards/cannonkeys/instant65/keymaps/tapdance/keymap.c
+++ keyboards/cannonkeys/instant65/keymaps/tapdance/keymap.c
@@ -27,8 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
-		KC_LCTL, MO(_1),   TD(TD_ALT_FN),                   KC_SPC,                      TT(_2),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
-
+		KC_LCTL, MO(_1),  LALT_T(MO(_5)),                   KC_SPC,                      TT(_2),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
     // arrow key layer
 	[_1] = LAYOUT_default(       
@@ -64,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 	// tap dance layer
 	[_5] = LAYOUT_default(
-		KC_NO,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_INS,
+		KC_TAB,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_INS,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
 		KC_LCTL, KC_NO,   KC_LALT,                   KC_SPC,                             KC_NO, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
-    ),
+    )
 };
